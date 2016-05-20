b5af50c2e13eb27bc48d3a9eece67d4c25ba8148
boris <polozovbv@gmail.com>
2016-05-20 11:55:36 +0300

    pizzashop

---
+++ chapter30/employees.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+
+class Employee:                     # Служащие
+    def __init__(self,name,salary=0):
+        self.name = name
+        self.salary = salary
+    def giveRaise(self,percent):    # Повышение
+        self.salary = self.salary + (self.salary * percent)
+    def work(self):                 # Работа
+        print(self.name, 'does stuff')
+    def __repr__(self):             # repr
+        return "Employee: name={0},salary={1:.0f}".format(self.name,self.salary)
+
+class Chef(Employee):               # Повар, который является служащим
+    def __init__(self,name):
+        Employee.__init__(self,name, 50000)
+    def work(self):                 # Повар умеет работать и делать еду :)
+        print(self.name, 'makes food')
+class Server(Employee):             # Официант
+    def __init__(self,name):
+        Employee.__init__(self,name, 40000)
+    def work(self):                 # Официант умеет работать с клиентами
+        print(self.name, 'interfaces with customer')
+class PizzaRobot(Chef):             # Робот является Поваром
+    def __init__(self,name):
+        Chef.__init__(self,name)
+    def work(self):
+        print(self.name, 'makes pizza')
+        
+if __name__ == '__main__':
+    bob = PizzaRobot('bob')
+    print(bob)
+    bob.work()
+    bob.giveRaise(0.20)
+    print(bob); print()
+    
+    for xclass in Employee,Chef,Server,PizzaRobot:        # Каждый из работников умеет что-то делать :)
+        obj = xclass(xclass.__name__)
+        obj.work()
---
+++ chapter30/pizza.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+from employees import PizzaRobot, Server # В магазине есть РоботПовар и Официант
+# А так же клиенты
+class Customer:                         # Клиент который имеет имя и умеет заказывать и платить
+    def __init__(self,name):
+        self.name = name
+    def order(self, server):            # Клиет заказывает
+        print(self.name, 'orders from', server)
+    def pay(self,server):               # Клиет платит :)
+        print(self.name, 'pays for item to', server)
+
+class Oven:                # Печь - печет :)
+    def bake(self):
+        print('oven bakes')
+
+class PizzaShop:           # Пицерия 
+    def __init__(self):
+        self.server = Server('Pat')     # Официант который возится с клиетом
+        self.chef = PizzaRobot('Bob')   # Повар который делает пицу
+        self.oven = Oven()
+    def order(self,name):               # Сцена заказа
+        customer = Customer(name)       # Берем имя клиента
+        customer.order(self.server)     # Клиент делает заказ у Pat
+        self.chef.work()                # Bob готовит
+        self.oven.bake()                # Печь - печет :)
+        customer.pay(self.server)       # Клиент платит :)
+
+if __name__ == '__main__':
+    scene = PizzaShop()                 # Сцена - Пицерия :)
+    scene.order('Homer')
+    print('...')
+    scene.order('Shaggy')
